e458376001df24ea523f72cba8b18a8252d4ae19
SuthiYuvaraj <92777079+SuthiYuvaraj@users.noreply.github.com>
2024-03-21 11:38:38 +0530

    Added sample for displaying a context menu in SfListView
    Added sample for displaying a context menu in SfListView

---
+++ ListViewMaui/ListViewMaui/MainPage.xaml.cs
@@ -0,0 +1,13 @@
+﻿namespace ListViewMaui
+{
+    public partial class MainPage : ContentPage
+    {
+        int count = 0;
+
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+}
